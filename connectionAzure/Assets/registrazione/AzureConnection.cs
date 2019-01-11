226c44e88d230a911352b351abe79ef0a67c9ca9
Mario Cuomo <cuomomario@hotmail.com>
2019-01-11 23:35:31 +0100

    First

---
+++ connectionAzure/Assets/registrazione/AzureConnection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AzureConnection : MonoBehaviour
+{
+
+        MobileServiceClient Client;
+        IMobileServiceTable<utenze> tbl;
+    
+        void Start()
+        {
+            Client = new MobileServiceClient("https://registrazioneunity.azurewebsites.net");
+        }
+
+
+        public async void Insert()
+        {
+            IMobileServiceTable<utenze> tbl = Client.GetTable<utenze>();
+
+            try
+            {
+                utenze utente = new utenze();
+                utente.Id = GameObject.Find("UsernameText").GetComponent<Text>().text;
+                utente.password = GameObject.Find("PasswordText").GetComponent<Text>().text;
+
+                await tbl.InsertAsync(utente);
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+}
---
+++ connectionAzure/Assets/registrazione/utenze.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class utenze
+{
+    public string Id { get; set; }
+
+    public string password { get; set; }
+
+}
